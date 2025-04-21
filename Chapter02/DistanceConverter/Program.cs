285a27ce6e2210091079800950958e36d289ae48
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-04-21 10:13:37 +0900

    コメント追加

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -2,6 +2,7 @@
 
 namespace DistanceConverter {
     internal class Program {
+        //コマンドライン引数で指定された範囲のフィートとメートルの対応表を出力する
         static void Main(string[] args) {
 
             int start = int.Parse(args[1]);
@@ -18,30 +19,27 @@ namespace DistanceConverter {
         //フィートからメートルへの対応表を出力
         static void PrintFeetToMeterList(int start, int end) {
             for (int feet = start; feet <= end; feet++) {
-                //double meter = feet * 0.3048;
                 double meter = FeetToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
         }
         //メートルからフィートへの対応表を出力
         static void PrintMeterToFeetList(int start, int end) {
-                for (int meter = start; meter <= end; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
-                }
-
-
-                //フィートからメートルへの変換
-                static double FeetToMeter(int feet) {
-                    return feet * 0.3048;
-                }
-
-                static double MeterToFeet(int meter) {
-                    return meter * 3.28084;
-                }
-
-
+            for (int meter = start; meter <= end; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
         }
+
+        //フィートからメートルへの変換
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        static double MeterToFeet(int meter) {
+            return meter * 3.28084;
+        }
+
+
     }
 }
