fc9c86a2499d5771f3990edfb9307478d7bdca04
infosys <infosys@C4210>
2025-04-21 09:58:03 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    //商品クラス
+    public class Product {
+        //商品コード
+        public int Code { get; private set; }       //プロパティ
+        //商品名
+        public string Name { get; private set;}
+        //商品価格（税抜き）
+        public int Price { get; private set;}
+
+
+        //消費税率10%
+        private readonly double _taxRate = 0.1;//フィールド
+
+        //消費税額を求める
+        public int GetTax() {                 //メソッド
+            return (int)(Price * _taxRate);
+        }
+
+        //コンストラクター
+        public Product (int Code,string Name,int Price) {
+            this.Code = Code;
+            this.Name = Name;
+            this.Price = Price;
+        }
+
+        
+        //税込み価格を求める
+        public int GetPriceIncludingTax() {
+            return Price + GetTax();
+        }
+    }
+}
