24455f1dc5b78fba06533be05f9316bbce73cbfd
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-04-23 09:44:12 +0900

    機能追加①完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,25 +1,53 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            //インチからメートルへの変換
-            for (int inch = 1; inch <= 10; inch++) {
+
+            Console.WriteLine("1:インチからメートルに変換");
+            Console.WriteLine("2:メートルからインチに変換");
+            int conversion = int.Parse(Console.ReadLine());
+            Console.Write("はじめ:");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("おわり:");
+            int end = int.Parse(Console.ReadLine());
+
+            if (conversion == 1) {
+
+                //メソッドの呼び出し
+                PrintinchToMeterList(start, end);
+
+            } else if (conversion == 2) {
+
+                //メソッドの呼び出し
+                PrintMeterToInchList(start, end);
+
+            } else {
+                Console.WriteLine("エラー");
+            }
+        }
+
+
+
+
+        //インチからメートルへの変換
+        static void PrintinchToMeterList(int start, int end) {
+            for (int inch = start; inch <= end; inch++) {
                 double meter = InchToMeter(inch);
                 Console.WriteLine($"{inch}inch = {meter:0.0000}m");
             }
-
-            static double InchToMeter(int inch) {
-                return inch * 0.0254;
-            }
-
-            //メートルからインチへの変換
-            for (int meter = 1; meter <= 10; meter++) {
+        }
+        //メートルからインチへの変換
+        static void PrintMeterToInchList(int start, int end) {
+            for (int meter = start; meter <= end; meter++) {
                 double inch = MeterToInch(meter);
                 Console.WriteLine($"{meter}m = {inch:0.0000}inch");
             }
+        }
+        static double InchToMeter(int inch) {
+            return inch * 0.0254;
+        }
 
-            static double MeterToInch(int meter) {
-                return meter / 0.0254;
-            }
+        static double MeterToInch(int meter) {
+            return meter / 0.0254;
         }
     }
 }
