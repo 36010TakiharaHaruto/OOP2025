3754050617d5287a667fb52e4505da31a7927428
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-05-07 09:36:37 +0900

    FormApps追加

---
+++ FormApps/StopWatch/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace StopWatch {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lbTimeDisp = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.tmDispTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbTimeDisp
+            // 
+            this.lbTimeDisp.BackColor = System.Drawing.Color.Yellow;
+            this.lbTimeDisp.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimeDisp.ForeColor = System.Drawing.Color.Red;
+            this.lbTimeDisp.Location = new System.Drawing.Point(12, 9);
+            this.lbTimeDisp.Name = "lbTimeDisp";
+            this.lbTimeDisp.Size = new System.Drawing.Size(338, 58);
+            this.lbTimeDisp.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            this.btStart.BackColor = System.Drawing.Color.Lime;
+            this.btStart.Font = new System.Drawing.Font("Pristina", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btStart.ForeColor = System.Drawing.Color.Blue;
+            this.btStart.Location = new System.Drawing.Point(18, 103);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(144, 43);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "start";
+            this.btStart.UseVisualStyleBackColor = false;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.BackColor = System.Drawing.Color.Red;
+            this.btStop.Font = new System.Drawing.Font("Pristina", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btStop.ForeColor = System.Drawing.Color.Yellow;
+            this.btStop.Location = new System.Drawing.Point(18, 203);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(144, 43);
+            this.btStop.TabIndex = 2;
+            this.btStop.Text = "stop";
+            this.btStop.UseVisualStyleBackColor = false;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // btReset
+            // 
+            this.btReset.BackColor = System.Drawing.Color.Cyan;
+            this.btReset.Font = new System.Drawing.Font("Pristina", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btReset.ForeColor = System.Drawing.Color.Magenta;
+            this.btReset.Location = new System.Drawing.Point(191, 103);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(144, 43);
+            this.btReset.TabIndex = 3;
+            this.btReset.Text = "reset";
+            this.btReset.UseVisualStyleBackColor = false;
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // tmDispTimer
+            // 
+            this.tmDispTimer.Tick += new System.EventHandler(this.tmDispTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(370, 311);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.lbTimeDisp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTimeDisp;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.Timer tmDispTimer;
+    }
+}
+
