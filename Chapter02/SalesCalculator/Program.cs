98976dcdb490781862a719c1f66cac3c66dc5e9c
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-05-09 15:59:25 +0900

    -暗黙の型指定

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -2,10 +2,10 @@
     internal class Program {
         static void Main(string[] args) {
             
-            SalesCounter sales = new SalesCounter(@"data\sales.csv");
+            var sales = new SalesCounter(@"data\sales.csv");
            
            IDictionary<string,int> amountsPerStore = sales.GetPerSales();
-            foreach (KeyValuePair<string, int> obj in amountsPerStore) {
+            foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
         }
