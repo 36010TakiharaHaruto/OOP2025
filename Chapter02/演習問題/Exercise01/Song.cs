60b82eca6a24222ca5c9e4255a64e85fe621f4ea
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-05-13 10:28:44 +0900

    2.1.1

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01{
+    //2.1.1
+  public  class Song{
+       public String Title { get; set; } 
+        public String ArtistName { get; set; } 
+        public int Length { get; set; } 
+        //2.1.2
+        
+    }
+
+
+
+}
