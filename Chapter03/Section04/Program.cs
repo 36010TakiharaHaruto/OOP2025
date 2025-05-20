13e3a7c63124524a41dfb56cb2a5ab3c4e359d9f
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-05-20 12:09:30 +0900

    3.1.3まで

--- Chapter03/Section04/Program.cs
+++ Chapter03/Section04/Program.cs
@@ -1,7 +1,40 @@
 ﻿namespace Section04 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+
+            //IEnumerable<string> query = cities
+            //    .Where(s => s.Length <= 5)    //条件に合ったもの抽出
+            //    .Select(s => s.ToLower());    //別オブジェクトへ変換(新しい方へ射影する)
+
+
+            var query = cities.Where(s => s.Length <= 5).ToArray();    //即時実行
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+
+            cities[0] = "Osaka";            //- cities[0]を変更
+            foreach (var item in query) {   //- 再度、queryの内容を取り出す【遅延実行】
+                Console.WriteLine(item);
+            }            
+
+
+
+
+
+
+
+
         }
     }
 }
