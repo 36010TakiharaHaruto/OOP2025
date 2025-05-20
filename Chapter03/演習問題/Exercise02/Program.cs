a081f9b95fdae310168653bf9b5092daca98f0e4
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-05-20 10:06:49 +0900

    04プロジェクト追加

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -40,7 +40,8 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_2(List<string> names) {
-            
+            var count = names.Count(s => s.ToString().Contains('o'));
+            Console.WriteLine(count);
         }
 
         private static void Exercise2_3(List<string> names) {
