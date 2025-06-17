9972f7350e3d5ed6e91638acc5981224bab0c9d5
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-06-17 11:34:28 +0900

    設問１_Student クラス完成

---
+++ Test01/Test01/ScoreCounter.cs
@@ -0,0 +1,39 @@
+﻿namespace Test01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
+        }
+
+        //メソッドの概要：科目別点数を求める
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            Dictionary<string,int> dict = new Dictionary<string, int>();
+            foreach (Student stu in _score) {
+                if (dict.ContainsKey(stu.Name))
+                    dict[stu.Name] += stu.Score;
+                else
+                    dict[stu.Name] = stu.Score;
+            }
+            return dict;
+        }
+
+        //メソッドの概要： 集計結果を出力する
+        public IDictionary<string, int> GetPerStudentScore() {
+        public static IEnumerable<Student> ReadSales(string filePath) {
+            var score = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+                var sco = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2]),
+                };
+                Student.Add(Student);
+            }
+            return Student;
+        }
+    }
+}
