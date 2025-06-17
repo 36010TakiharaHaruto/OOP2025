b4abdd832a6a64ccad21482dca901ed912d13e91
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-06-17 09:45:30 +0900

    問題8－2準備

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            
+
             var sales = new SalesCounter(@"data\sales.csv");
-           
-           IDictionary<string,int> amountsPerStore = sales.GetPerSales();
+
+            IDictionary<string, int> amountsPerStore = sales.GetPerSales();
             foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
