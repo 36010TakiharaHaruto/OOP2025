099b2a32e5d3385d0b886b5a91830d4613c462b7
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-07-18 14:07:12 +0900

    戻るボタンと進むボタン

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -26,26 +26,30 @@
             tbUrl = new TextBox();
             btRssGet = new Button();
             lbTitles = new ListBox();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            btMove = new Button();
+            btReturn = new Button();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
             SuspendLayout();
             // 
             // tbUrl
             // 
             tbUrl.Font = new Font("Yu Gothic UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            tbUrl.Location = new Point(12, 12);
+            tbUrl.Location = new Point(250, 15);
             tbUrl.Name = "tbUrl";
-            tbUrl.Size = new Size(422, 35);
+            tbUrl.Size = new Size(457, 35);
             tbUrl.TabIndex = 0;
             // 
             // btRssGet
             // 
             btRssGet.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            btRssGet.Location = new Point(450, 12);
+            btRssGet.Location = new Point(734, 14);
             btRssGet.Name = "btRssGet";
             btRssGet.Size = new Size(76, 35);
             btRssGet.TabIndex = 1;
             btRssGet.Text = "取得";
             btRssGet.UseVisualStyleBackColor = true;
-            btRssGet.Click += btRssGet_Click;
+            btRssGet.Click += btRssGet_Click_1;
             // 
             // lbTitles
             // 
@@ -55,19 +59,55 @@
             lbTitles.ItemHeight = 21;
             lbTitles.Location = new Point(12, 63);
             lbTitles.Name = "lbTitles";
-            lbTitles.Size = new Size(514, 445);
+            lbTitles.Size = new Size(990, 256);
             lbTitles.TabIndex = 2;
+            lbTitles.Click += lbTitles_Click;
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(12, 325);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(990, 312);
+            webView21.TabIndex = 3;
+            webView21.ZoomFactor = 1D;
+            // 
+            // btMove
+            // 
+            btMove.Location = new Point(126, 14);
+            btMove.Name = "btMove";
+            btMove.Size = new Size(86, 36);
+            btMove.TabIndex = 4;
+            btMove.Text = "進む";
+            btMove.UseVisualStyleBackColor = true;
+            btMove.Click += btMove_Click;
+            // 
+            // btReturn
+            // 
+            btReturn.Location = new Point(12, 15);
+            btReturn.Name = "btReturn";
+            btReturn.Size = new Size(90, 35);
+            btReturn.TabIndex = 5;
+            btReturn.Text = "戻る";
+            btReturn.UseVisualStyleBackColor = true;
+            btReturn.Click += btReturn_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(621, 541);
+            ClientSize = new Size(1065, 763);
+            Controls.Add(btReturn);
+            Controls.Add(btMove);
+            Controls.Add(webView21);
             Controls.Add(lbTitles);
             Controls.Add(btRssGet);
             Controls.Add(tbUrl);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -77,5 +117,8 @@
         private TextBox tbUrl;
         private Button btRssGet;
         private ListBox lbTitles;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private Button btMove;
+        private Button btReturn;
     }
 }
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace RssReader {
@@ -10,23 +11,54 @@ namespace RssReader {
             InitializeComponent();
         }
 
-        private void btRssGet_Click(object sender, EventArgs e) {
-            using (var wc = new WebClient()) {
-                var url = wc.OpenRead(tbUrl.Text);
-                XDocument xdoc = XDocument.Load(url); //RSSの取得
+        private async void btRssGet_Click_1(object sender, EventArgs e) {
+            using (var hc = new HttpClient()) {
+                string xml = await hc.GetStringAsync(tbUrl.Text);
+                XDocument xdoc = XDocument.Parse(xml);
 
+
+
+                //var url = hc.OpenRead(tbUrl.Text);
+                //XDocument xdoc = XDocument.Load(url); //RSSの取得
 
                 //RSSを解析して必要な要素を取得
                 items = xdoc.Root.Descendants("item")
-                    .Select(x => new ItemData { Title = (string)x.Element("title"), })
-                                              .ToList();
+                    .Select(x => new ItemData {
+                        Title = (string?)x.Element("title")
+                                                ,
+                        Link = (string?)x.Element("link")
+                    }).ToList();
 
 
                 //リストボックスへタイトルを表示
                 lbTitles.Items.Clear();
-                foreach (var item in items) {
-                    lbTitles.Items.Add(item.Title);
+                items.ForEach(item => lbTitles.Items.Add(item.Title ?? "データなし"));
+                //foreach (var item in items) {
+                //    lbTitles.Items.Add(item.Title);
+                //}
+            }
+        }
+
+        //タイトルを検索（クリック）したときに呼ばれるイベントハンドラ
+        private void lbTitles_Click(object sender, EventArgs e) {
+            int index = lbTitles.SelectedIndex;
+            if (index >= 0 && index < items.Count) {
+                string? link = items[index].Link;
+                if (!string.IsNullOrEmpty(link)) {
+                    webView21.Source = new Uri(link);
                 }
+            }
+        }
+
+        private void btReturn_Click(object sender, EventArgs e) {
+            if (webView21.CanGoBack) {
+                webView21.GoBack();
+            }
+        }
+
+        private void btMove_Click(object sender, EventArgs e) {
+            if (webView21.CanGoForward) {
+                webView21.GoForward();
             }
         }
     }
