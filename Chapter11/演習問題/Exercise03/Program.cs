4c7503f28e6a503032c5eb784f3af69c2856458e
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-09-02 13:23:14 +0900

    11.5

---
+++ Chapter11/演習問題/Exercise03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] texts = [
+                "Time is money.",
+                "What time is it?",
+                "It will take time.",
+                "We reorganized the timetable.",
+            ];
+            //Regex regex = new Regex(@"time", RegexOptions.IgnoreCase);
+            //foreach (var line in texts) {
+            //    var matches = regex.Matches(line);
+            //    if (matches.Count > 0) {
+            //        Console.WriteLine(line);
+            //        foreach (Match match in matches) {
+            //            Console.WriteLine(match.Index);
+            //        }
+            foreach (var line in texts) {
+                var matches = Regex.Matches(line, @"\btime\b", RegexOptions.IgnoreCase);
+                
+                foreach (Match match2 in matches) {
+                    Console.WriteLine($"{line},{match2.Index}");
+                }
+            }
+        }
+    }
+}
---
+++ Chapter11/演習問題/Exercise04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var lines = File.ReadAllLines("sample.txt");
+            //問題11.4
+            var newlines = lines.Select(s => Regex.Replace(s, "version=\"v4\\.0\"", "version=\"v5.0\""));
+
+            File.WriteAllLines("sampleChange.txt",newlines);
+
+            //これ以降は確認用
+            var text = File.ReadAllText("sampleChange.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
