0e4d2c1578996d254e5d96eebc1ba7ed842d0d18
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-10-22 10:24:06 +0900

    画像保存

---
+++ SQLite/CustomerApp/Data/Customer.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CustomerApp.Data {
+    internal class Customer {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// 電話番号
+        /// </summary>
+        public string Phone { get; set; } = string.Empty;
+        /// <summary>
+        /// 住所
+        /// </summary>
+        public string Address { get; set; } = string.Empty;
+        /// <summary>
+        /// 画像
+        /// </summary>
+        public byte[] Picture { get; set; }
+
+        public BitmapImage PictureImage {
+            get {
+                if (Picture == null || Picture.Length == 0) return null;
+                var image = new BitmapImage();
+                using var stream = new MemoryStream(Picture);
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = stream;
+                image.EndInit();
+                return image;
+            }
+        }
+    }
+}
---
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using SQLite;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using CustomerApp.Data;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+
+namespace CustomerApp {
+    public partial class App : Application {
+        public static string databasePath;
+
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            var folder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "CustomerApp");
+
+            if (!Directory.Exists(folder)) {
+                Directory.CreateDirectory(folder);
+            }
+
+            databasePath = Path.Combine(folder, "customers.db");
+        }
+    }
+
+    public partial class MainWindow : Window {
+        private List<Customer> _customers = new();
+        private byte[] _selectedImageBytes;
+
+        public MainWindow() {
+            InitializeComponent();
+            ReadDatabase();
+            PersonListView.ItemsSource = _customers;
+        }
+
+        private void ReadDatabase() {
+            using var connection = new SQLiteConnection(App.databasePath);
+            connection.CreateTable<Customer>();
+            _customers = connection.Table<Customer>().ToList();
+        }
+
+        private void PictureButton_Click(object sender, RoutedEventArgs e) {
+            var dialog = new OpenFileDialog {
+                Title = "画像を選択",
+                Filter = "画像ファイル (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg"
+            };
+
+            if (dialog.ShowDialog() == true) {
+                _selectedImageBytes = File.ReadAllBytes(dialog.FileName);
+                MessageBox.Show("画像が選択されました");
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e) {
+            var customer = new Customer {
+                Name = NameTextBox.Text,
+                Phone = PhoneTextBox.Text,
+                Address = AddressTextBox.Text,
+                Picture = _selectedImageBytes
+            };
+
+            using var connection = new SQLiteConnection(App.databasePath);
+            connection.CreateTable<Customer>();
+            connection.Insert(customer);
+
+            _selectedImageBytes = null;
+            ReadDatabase();
+            PersonListView.ItemsSource = _customers;
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e) {
+            var selectedCustomer = PersonListView.SelectedItem as Customer;
+            if (selectedCustomer == null) return;
+
+            selectedCustomer.Name = NameTextBox.Text;
+            selectedCustomer.Phone = PhoneTextBox.Text;
+            selectedCustomer.Address = AddressTextBox.Text;
+
+            if (_selectedImageBytes != null) {
+                selectedCustomer.Picture = _selectedImageBytes;
+            }
+
+            using var connection = new SQLiteConnection(App.databasePath);
+            connection.CreateTable<Customer>();
+            connection.Update(selectedCustomer);
+
+            _selectedImageBytes = null;
+            ReadDatabase();
+            PersonListView.ItemsSource = _customers;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            var selectedCustomer = PersonListView.SelectedItem as Customer;
+            if (selectedCustomer == null) {
+                MessageBox.Show("行を選択してください");
+                return;
+            }
+
+            using var connection = new SQLiteConnection(App.databasePath);
+            connection.CreateTable<Customer>();
+            connection.Delete(selectedCustomer);
+
+            ReadDatabase();
+            PersonListView.ItemsSource = _customers;
+        }
+
+        private void SerchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            var keyword = SearchTextBox.Text.ToLower();
+            var filtered = _customers.Where(x =>
+                x.Name.ToLower().Contains(keyword) ||
+                x.Phone.Contains(keyword) ||
+                x.Address.ToLower().Contains(keyword)).ToList();
+
+            PersonListView.ItemsSource = filtered;
+        }
+
+        private void PersonListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selectedCustomer = PersonListView.SelectedItem as Customer;
+            if (selectedCustomer == null) return;
+
+            NameTextBox.Text = selectedCustomer.Name;
+            PhoneTextBox.Text = selectedCustomer.Phone;
+            AddressTextBox.Text = selectedCustomer.Address;
+        }
+    }
+}
