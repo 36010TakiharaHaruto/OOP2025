7f237bbae4ba37efa3c83a486c44eca54a5cf185
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-11-05 11:16:38 +0900

    コード14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient();
+            await GetHtmlExample(hc); 
+        }
+
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://www.google.com/?hl=ja";
+            var text = await httpClient.GetStreamAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
