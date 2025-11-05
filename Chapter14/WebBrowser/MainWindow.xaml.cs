94b47918eacf9258dd838369216d28817c3bfcf6
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-11-05 12:08:41 +0900

    WebBrowser完成

---
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Wpf;
+
+namespace WebBrowser {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            InitializeWebView();
+        }
+        private async void InitializeWebView() {
+            await WebView.EnsureCoreWebView2Async(null);
+            WebView.CoreWebView2.Navigate("https://www.example.com");
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e) {
+            if (WebView.CoreWebView2.CanGoBack) {
+                WebView.CoreWebView2.GoBack();
+            }
+        }
+        private void FowardButton_Click(object sender, RoutedEventArgs e) {
+            if (WebView.CoreWebView2.CanGoForward) {
+                WebView.CoreWebView2.GoForward();
+            }
+        }
+        private void GoButton_Click(object sender, RoutedEventArgs e) {
+            string url = AddressBar.Text;
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute)) {
+                WebView.CoreWebView2.Navigate(url);
+            } else {
+                MessageBox.Show("無効なURLです。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
