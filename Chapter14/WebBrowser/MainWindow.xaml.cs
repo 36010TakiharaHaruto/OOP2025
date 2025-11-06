bf4f30bb3c52fe9a9975834068bf8d324ef67e80
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-11-06 14:29:30 +0900

    完成

--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -8,12 +8,33 @@ namespace WebBrowser {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            InitializeWebView();
+            InitializeAsync();
+            //InitializeWebView();
         }
-        private async void InitializeWebView() {
-            await WebView.EnsureCoreWebView2Async(null);
-            WebView.CoreWebView2.Navigate("https://www.example.com");
+        //private async void InitializeWebView() {
+        //    await WebView.EnsureCoreWebView2Async(null);
+        //    WebView.CoreWebView2.Navigate("https://www.example.com");
+        //}
+        private async void InitializeAsync() {
+            await WebView.EnsureCoreWebView2Async();
+
+            WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+            WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
         }
+
+        // 読み込み開始したらプログレスバーを表示
+        private void CoreWebView2_NavigationStarting(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e) {
+            LoadingBar.Visibility = Visibility.Visible;
+            LoadingBar.IsIndeterminate = true;
+        }
+
+        // 読み込み完了したらプログレスバーを非表示
+        private void CoreWebView2_NavigationCompleted(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e) {
+            LoadingBar.Visibility = Visibility.Collapsed;
+            LoadingBar.IsIndeterminate = true;
+        }
+
+
         private void BackButton_Click(object sender, RoutedEventArgs e) {
             if (WebView.CoreWebView2.CanGoBack) {
                 WebView.CoreWebView2.GoBack();
@@ -25,12 +46,14 @@ namespace WebBrowser {
             }
         }
         private void GoButton_Click(object sender, RoutedEventArgs e) {
-            string url = AddressBar.Text;
-            if (Uri.IsWellFormedUriString(url, UriKind.Absolute)) {
-                WebView.CoreWebView2.Navigate(url);
-            } else {
-                MessageBox.Show("無効なURLです。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            //string url = AddressBar.Text;
+            //if (Uri.IsWellFormedUriString(url, UriKind.Absolute)) {
+            //    WebView.CoreWebView2.Navigate(url);
+            //} else {
+            //    MessageBox.Show("無効なURLです。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+            var url = AddressBar.Text.Trim();
+            if (string.IsNullOrWhiteSpace(url)) return;
         }
     }
 }
