9e39258c2d2a25d533ff37eb7727629610d29bda
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-11-10 15:28:42 +0900

    Chapter10

---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+class Exercise01
+    {
+    static void Main() {
+        Console.Write("読み込むC#ファイル名を入力してください: ");
+        string filePath = Console.ReadLine();
+        if (!File.Exists(filePath)) {
+            Console.WriteLine("ファイルが見つかりません。");
+            return;
+        }
+        var lines = File.ReadLines(filePath);
+        int count = 0;
+        foreach (var line in lines) {
+            if (line.Contains("class")) {
+
+                count++;
+
+            }
+
+        }
+        Console.WriteLine($"'class' は {count} 回出現しました。");
+    }
+
+}
