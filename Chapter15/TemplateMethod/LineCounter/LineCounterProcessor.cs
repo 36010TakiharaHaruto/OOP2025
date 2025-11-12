6104adc75fe91f4a4013e6c1ad65d33c433c6246
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-11-12 11:46:31 +0900

    2番完成

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using TextFileProcessor;
@@ -8,14 +9,22 @@ using TextFileProcessor;
 namespace LineCounter {
     internal class LineCounterProcessor : TextProcessor {
         private int _count = 0;
+        private string _keyword = "";
 
-        protected override void Initialize(string fname) => _count = 0;
-
-        protected override void Execute(string line) {
-            if (line.Contains("public"))
-                _count++;
+        protected override void Initialize(string fname) {
+            Console.Write("単語を入力: ");
+            _keyword = Console.ReadLine();
+            _count = 0;
         }
 
-        protected override void Terminate() => Console.WriteLine("{0} 個", _count);
+        protected override void Execute(string line) {
+            int index = 0;
+            while ((index = line.IndexOf(_keyword, index)) != -1) {
+                _count++;
+                index += _keyword.Length;
+            }
+        }
+
+        protected override void Terminate() => Console.WriteLine("\"{0}\" の出現回数: {1} 個", _keyword, _count);
     }
 }
