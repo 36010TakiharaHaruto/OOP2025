118dded36aecd85b734939f4f9863d8732a48395
TakiharaHaruto <oibc36010@ojs.ac.jp>
2025-11-13 13:50:59 +0900

    パス指定の単語カウント

--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -1,8 +1,23 @@
-﻿using TextFileProcessor;
+﻿using System;
+using TextFileProcessor;
+
 namespace LineCounter {
     internal class Program {
         static void Main(string[] args) {
-            TextProcessor.Run<LineCounterProcessor>(args[0]);
+            string path;
+            if (args.Length > 0) {
+                path = args[0];
+            }
+            else {
+                Console.Write("検索するファイルのパスを入力してください: ");
+                path = Console.ReadLine();
+            }
+            path = path.Trim().Trim('"');
+            if (string.IsNullOrWhiteSpace(path) || !File.Exists(path)) {
+                Console.WriteLine("有効なファイルパスを指定してください。");
+                return;
+            }
+            TextProcessor.Run<LineCounterProcessor>(path);
         }
     }
 }
